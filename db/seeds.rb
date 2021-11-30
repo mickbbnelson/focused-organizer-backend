a647787b0b33d031a3e568db02262b0bd70ad062
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-11-30 16:51:54 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+trash = Task.create(task: 'Take out the trash', priority: 'Low',  category: 'Home' , notes: 'Take out to the curb every tuesday night')
+receivables = Task.create(task: 'Receivables Report', priority: 'High',  category: 'Work' , notes: 'Need to send before noon tomorrow')
+blog_post = Task.create(task: 'Blog Post', priority: 'High',  category: 'School' , notes: 'Finish final draft and post')
+pay_bills = Task.create(task: 'Pay Bills', priority: 'Medium',  category: 'Home' , notes: 'Need all but water bill paid by Friday')
+finish_labs = Task.create(task: 'Weekly Labs', priority: 'Medium',  category: 'School' , notes: 'Finish labs by Sunday')
